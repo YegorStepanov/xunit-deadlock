786724d9cd30bfd44898735f873fe27e49ee1db9
Yegor Stepanov <yegor.stepanov@outlook.com>
2022-09-29 13:37:06 +0300

    log

---
+++ MyTests2.cs
@@ -0,0 +1,58 @@
+﻿using Xunit.Abstractions;
+
+namespace xunit_deadlock;
+
+public class MyTests2
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+    public MyTests2(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    public void Method2()
+    {
+        _testOutputHelper.WriteLine("MyMethod2");
+        var task = new TestClass2().GlobalSetup();
+        var isAsyncMethod = MyTests1.TryAwaitTask(task, out var result);
+
+        Assert.True(isAsyncMethod);
+        Assert.Equal(42, result);
+        Assert.True(TestClass2.WasCalled);
+    }
+
+    public class TestClass2
+    {
+        public static bool WasCalled;
+
+        public async Task<int> GlobalSetup()
+        {
+            await Task.Delay(1);
+            WasCalled = true;
+            return 42;
+        }
+    }
+
+    [Fact]
+    public void Method3()
+    {
+        _testOutputHelper.WriteLine("MyMethod3");
+        var task = new TestClass3().GlobalCleanup();
+        var isAsyncMethod = MyTests1.TryAwaitTask(task, out var result);
+
+        Assert.True(isAsyncMethod);
+        Assert.True(TestClass3.WasCalled);
+    }
+
+    public class TestClass3
+    {
+        public static bool WasCalled;
+
+        public async ValueTask GlobalCleanup()
+        {
+            await Task.Delay(1);
+            WasCalled = true;
+        }
+    }
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,12 +1,20 @@
 using System.Reflection;
+using Xunit.Abstractions;
 
 namespace xunit_deadlock;
 
 public class MyTests1
 {
+    private readonly ITestOutputHelper _testOutputHelper;
+    public MyTests1(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
     [Fact]
     public void Method1()
     {
+        _testOutputHelper.WriteLine("MyMethod1");
         var task = new TestClass1().Foo();
         var isAsyncMethod = TryAwaitTask(task, out var result);
 
@@ -75,52 +83,5 @@ public class MyTests1
 
         result = property.GetValue(task);
         return true;
-    }
-}
-
-public class MyTests2
-{
-    [Fact]
-    public void Method2()
-    {
-        var task = new TestClass2().GlobalSetup();
-        var isAsyncMethod = MyTests1.TryAwaitTask(task, out var result);
-
-        Assert.True(isAsyncMethod);
-        Assert.Equal(42, result);
-        Assert.True(TestClass2.WasCalled);
-    }
-
-    public class TestClass2
-    {
-        public static bool WasCalled;
-
-        public async Task<int> GlobalSetup()
-        {
-            await Task.Delay(1);
-            WasCalled = true;
-            return 42;
-        }
-    }
-
-    [Fact]
-    public void Method3()
-    {
-        var task = new TestClass3().GlobalCleanup();
-        var isAsyncMethod = MyTests1.TryAwaitTask(task, out var result);
-
-        Assert.True(isAsyncMethod);
-        Assert.True(TestClass3.WasCalled);
-    }
-
-    public class TestClass3
-    {
-        public static bool WasCalled;
-
-        public async ValueTask GlobalCleanup()
-        {
-            await Task.Delay(1);
-            WasCalled = true;
-        }
     }
 }
